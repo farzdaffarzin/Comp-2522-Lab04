7e6353c8dfc9b8ee716be401b21d89a0fad44b49
Anil Bronson <anilbronson12@gmail.com>
2024-09-26 10:02:29 -0700

    finished name and printable

--- untitled/src/code/ca/bcit/comp2522/lab4/Name.java
+++ untitled/src/code/ca/bcit/comp2522/lab4/Name.java
@@ -1,4 +1,40 @@
 package ca.bcit.comp2522.lab4;
 
-public class Name {
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author Farzad Farzin
+ * @author Anil Bronson
+ * @author Dalraj Bains
+ * @author Arsh Mann
+ * @version 1.0
+ */
+public class Name implements Printable {
+
+    private final String firstName;
+    private final String lastName;
+
+    /**
+     * Constructor for the Name class.
+     *
+     * @param firstName of the person
+     * @param lastName of the person
+     */
+    Name(final String firstName,
+         final String lastName) {
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    /**
+     * Uses the Printable interface
+     * @return the full name of the person
+     */
+    @Override
+    public String display(){
+        return firstName + " " + lastName;
+    }
 }
--- untitled/src/code/ca/bcit/comp2522/lab4/Printable.java
+++ untitled/src/code/ca/bcit/comp2522/lab4/Printable.java
@@ -1,4 +1,9 @@
 package ca.bcit.comp2522.lab4;
 
 public interface Printable {
+
+    /**
+     * prints every instance variable (including those in parent classes) in a sentence.
+     */
+    public String display();
 }
